18314c412014b84e3875d7a2d9741d814b21a65f
Matt Newton <newton10471@gmail.com>
2012-01-16 18:41:31 -0500

    Done with sign in

--- Gemfile
+++ Gemfile
@@ -9,12 +9,12 @@ gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'gravatar_image_tag'
 
 group :development do
-  gem 'rspec-rails'
+  gem 'rspec-rails', '2.8.1'
   gem 'annotate'
 end
 
 group :test do
-  gem 'rspec'
+  gem 'rspec', '2.8.0'
   gem 'webrat'
   gem 'spork'
   gem 'factory_girl_rails'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,22 @@
-require 'rubygems'
-require 'spork'
-
-Spork.prefork do
+# require 'rubygems'
+# require 'spork'
+# 
+# Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However, 
   # if you change any configuration or code from libraries loaded here, you'll
   # need to restart spork for it take effect.
-  ENV["RAILS_ENV"] ||= 'test'
-  unless defined?(Rails)
-    require File.dirname(__FILE__) + "/../config/environment"
-  end
-  
-  require 'rspec/rails'
+#   ENV["RAILS_ENV"] ||= 'test'
+#   unless defined?(Rails)
+#     require File.dirname(__FILE__) + "/../config/environment"
+#   end
+#   
+#   require 'rspec/rails'
 
   # Requires supporting files with custom matchers and macros, etc,
   # in ./support/ and its subdirectories.
-  Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-  
-  Rspec.configure do |config|
+#   Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+#   
+#   Rspec.configure do |config|
     # == Mock Framework
     #
     # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -24,26 +24,26 @@ Spork.prefork do
     # config.mock_with :mocha
     # config.mock_with :flexmock
     # config.mock_with :rr
-    config.mock_with :rspec
-    
-    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+#     config.mock_with :rspec
+#     
+#     config.fixture_path = "#{::Rails.root}/spec/fixtures"
     
     # If you're not using ActiveRecord, or you'd prefer not to run each of your
     # examples within a transaction, comment the following line or assign false
     # instead of true.
-    config.use_transactional_fixtures = true
+#    config.use_transactional_fixtures = true
     
     ### Part of a Spork hack. See http://bit.ly/arY19y
     # Emulate initializer set_clear_dependencies_hook in
     # railties/lib/rails/application/bootstrap.rb
-    ActiveSupport::Dependencies.clear
-  end
-end
-
-Spork.each_run do
+#     ActiveSupport::Dependencies.clear
+#   end
+# end
+# 
+# Spork.each_run do
   # This code will be run each time you run your specs.
   
-end
+#end
 
 # --- Instructions ---
 # - Sort through your spec_helper file. Place as much environment loading 
@@ -91,4 +91,8 @@ RSpec.configure do |config|
   # automatically. This will be the default behavior in future versions of
   # rspec-rails.
   config.infer_base_class_for_anonymous_controllers = false
+  
+  def test_sign_in(user)
+		controller.sign_in(user)
+	end
 end
